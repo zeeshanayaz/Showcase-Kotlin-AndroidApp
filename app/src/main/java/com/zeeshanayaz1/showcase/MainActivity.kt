75ee2bbeb6bddb4f0fbab20aac758445e13a310c
Zeeshan Ayaz <zeeshanayaz1@gmail.com>
2018-08-10 19:23:24 +0500

    Showcase Android app in Kotlin

---
+++ app/src/main/java/com/zeeshanayaz1/showcase/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.zeeshanayaz1.showcase
+
+import android.graphics.BitmapFactory
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 15f
+//        rounded.isCircular = true
+        imageViewLogo.setImageDrawable(rounded)
+
+//        imageViewImage.setImageDrawable(rounded)
+//        imageViewVideo.setImageDrawable(rounded)
+//        imageViewData.setImageDrawable(rounded)
+
+    }
+}
